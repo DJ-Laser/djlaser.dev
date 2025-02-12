d4146aa0ed11f24cbb53914abcd636f47662ed97
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-02-12 09:45:30 -0500

    fire site

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { Logo } from "./Logo";
-import viteLogo from "/vite.svg";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="flex flex-row flex-nowrap justify-evenly">
-        <span className="transition-[filter] duration-300 hover:drop-shadow-[0_0_2rem_#646cffaa]">
-          <Logo href="https://vite.dev" src={viteLogo} alt="Vite logo" />
-        </span>
-        <span className="transition-[filter] duration-300 hover:drop-shadow-[0_0_2rem_#61dafbaa] motion-safe:animate-[spin_20s_linear_infinite]">
-          <Logo href="https://react.dev" src={reactLogo} alt="React logo" />
-        </span>
-      </div>
-      <h1 className="my-5 font-bold text-5xl">Vite + React</h1>
-      <div className="p-8">
-        <button
-          className="px-5 py-2 rounded-lg bg-neutral-900 border border-transparent hover:border-cyan-400 cursor-pointer transition-colors duration-250"
-          onClick={() => setCount((count) => count + 1)}
+      <h1 className="my-5 font-bold text-6xl">DJ_Laser</h1>
+      <h2 className="my-5 font-bold text-3xl">
+        I do silly programmimng things sometimes
+      </h2>
+      <h3 className="my-5 font-bold text-5xl">Projects</h3>
+      <p>
+        man i dont know, just scroll through my{" "}
+        <a
+          className="text-cyan-500 hover:text-cyan-600"
+          href="https://github.com/dj-laser"
         >
-          count is {count}
-        </button>
-        <p className="mt-4">
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-neutral-300">
-        Click on the Vite and React logos to learn more
+          github
+        </a>{" "}
+        to find all my unfinisghed stuff bc i cant be bothred to update this
+        site
       </p>
     </>
   );
